bb9a7dfc1721356ffed26cef9bd8f6f3e9734304
Hung <khanhhungvo001@gmail.com>
2016-06-28 16:44:21 +0700

    new update

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
+	validates :body, presence: true
+	validates :title, presence: true
 	def self.search(search)
 	  where("title ILIKE ?", "%#{search}%") 
-	  where("body ILIKE ?", "%#{search}%")
+	  # where("body ILIKE ?", "%#{search}%")
 	end
 end
